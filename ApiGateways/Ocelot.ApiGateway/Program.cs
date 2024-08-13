8a98b54d6dcdc6410586eac4da336f246f53c29c
AbdulHCore <fohakeem@gmail.com>
2024-08-13 16:51:30 +0100

    Ocelot ApiGateway Added

---
+++ ApiGateways/Ocelot.ApiGateway/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ocelot.ApiGateway
+{
+    public class Program
+    {
+        public static void main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                    .ConfigureAppConfiguration((env, config) =>
+                    {
+                        config.AddJsonFile($"ocelot.{env.HostingEnvironment.EnvironmentName}.json", true, true);
+                    }).ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                    });
+        }
+    }
+}
